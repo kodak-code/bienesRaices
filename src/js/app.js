4d6a302b2201a40f6c029e2b91779cfeaff31173
kodak-code <mamanicamaramatias@industrialdetemperley.edu.ar>
2024-12-12 00:51:03 -0300

    modificaciones para el modo oscuro

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    eventListeners();
+    menu();
+    modoOscuro();
 
 });
-function eventListeners() {
-
+function menu() {
     const menuMovil = document.querySelector('.mobile-menu');
-    menuMovil.addEventListener('click', navegacionResponcive);
 
+    menuMovil.addEventListener('click', function () {
+        const navegacion = document.querySelector('.navegacion');
+        navegacion.classList.toggle('mostrar');
+    });
 }
 
-function navegacionResponcive() {
-    const navegacion = document.querySelector('.navegacion');
-    navegacion.classList.toggle('mostrar');
+function modoOscuro() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    console.log(prefiereDarkMode.matches);
+
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode')
+    }
+
+    prefiereDarkMode.addEventListener('change', function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode')
+        }
+    });
+
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+    });
 }
